b62b70b2a8e063013d86119fb648f85c0a276e9f
Jason Cauwels <troutacular@gmail.com>
2015-12-04 11:34:45 -0800

    shortcode for event types

--- shortcodes/shortcodes.localist.php
+++ shortcodes/shortcodes.localist.php
@@ -9,25 +9,84 @@
  * Add the shortcode for Localist Widget
  * 
  * @since 1.0.0
- * @param string name pass the name
- * @return html 	events list
  * 
- * usage: [locaclist src="widget value"]
+ * @require $localist_config array
+ * @param 	string 	params 	shortcode api options
+ * @return 	html 			events list
+ * 
+ * usage: [localist-events {option=value}]
  */
-function usc_localist_fwp_shortcodes( $params ) {
-	// get the attributes from the shortcode
-	$p = shortcode_atts( array(
-		'name' => 'default value'
-	), $params );
+function usc_localist_fwp_events_shortcode( $params ) {
+	
+	// get the global configuration
+	global $localist_config;
+
+	// function variables
+		$pagination_name = $localist_config['url']['variables']['pagination'];
+
+		// get pagination from url
+		$page = filter_input(INPUT_GET, $pagination_name, FILTER_SANITIZE_NUMBER_INT);
+
+	
+	// get all api options
+	$attr_all = shortcode_atts($localist_config['api_options']['all']['allowed'], $params, 'localist-calendar');
+
+	// store the api type as a variable
+	$api_type = $attr_all['get'];
+
+	// check that we have a valid 'get' type
+	if ( $api_type == '' || $api_type == null ) {
+
+		// let's default to events
+		$api_type = 'events';
+
+	}
+
+	// get the available api options (based on type) from the shortcode
+	$api_attr = shortcode_atts( $localist_config['api_options'][$api_type]['allowed'], $params, 'localist-calendar' );
+
+	// build the api string
+	$json_url = array(
+		'type' => $api_type
+		//'url' => plugins_url( '/sample/events.json', dirname(__FILE__) )
+	);
+
+	// get the matching api options by get type_url_form_file()
+	$parameters_string = parameters_as_string($api_attr, $api_type);
+
+	if ( !empty( $parameters_string ) ) {
+		$json_url['options'] = $parameters_string;
+	}
+
+	//var_dump( $parameters_string );
 
 	// return values as $p['name']
 
 	// function to get the json data from the server - store as transient
 
-	// return the output using the parameters
+	//var_dump($json_url);
+	$json_data = get_json($json_url);
+
+	if ( $json_data ) {
+		
+		
+		// TODO: function for looping through json data
+		if ( is_array( $json_data ) ) {
+
+		} else if ( $json_data != null || $json_data != '' ) {
+
+			// output any error messages
+			echo $json_data;
+
+		} else {
+
+			// output message that something needs to be addressed
+			_e('Ninja wildebeests have sprung into action. Please contact plugin development team.','textdomain');
+		}
+	}
 }
 
 // add the shortcode function
-add_shortcode( 'localist', 'usc_localist_fwp_shortcodes' );
+add_shortcode( 'localist-calendar', 'usc_localist_fwp_events_shortcode' );
 
 ?>
