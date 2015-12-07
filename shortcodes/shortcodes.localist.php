45870a9bcf5829a3d133cbeff96998dc8e2fe957
Jason Cauwels <troutacular@gmail.com>
2015-12-07 10:56:44 -0800

    shortcode clean up and better error handling

--- shortcodes/shortcodes.localist.php
+++ shortcodes/shortcodes.localist.php
@@ -21,67 +21,85 @@ function usc_localist_fwp_events_shortcode( $params ) {
 	// get the global configuration
 	global $localist_config;
 
-	// function variables
-		$pagination_name = $localist_config['url']['variables']['pagination'];
+	// default setting for error checking
+	$errors = $json_data = false;
 
-		// get pagination from url
-		$page = filter_input( INPUT_GET, $pagination_name, FILTER_SANITIZE_NUMBER_INT );
+	// get api type
 
+		// get all api options
+		$attr_all = shortcode_atts( $localist_config['api_options']['all']['allowed'], $params, 'localist-calendar' );
+
+		// store the api type as a variable
+		$api_type = $attr_all['get'];
+
+		// check that we have a valid 'get' type
+		if ( $api_type == '' || $api_type == null ) {
+
+			// let's default to events
+			$api_type = 'events';
+
+		}
+
+	// get allowed api attributes
+
+		// get the available api options (based on type) from the shortcode
+		$api_attr = shortcode_atts( $localist_config['api_options'][$api_type]['allowed'], $params, 'localist-calendar' );
+
+		// build the api string
+		$json_url = array(
+			'type' => $api_type
+		);
+
+	// build the api url string for any options
+
+		// get the matching api options by get type_url_form_file()
+		$parameters_string = usc_localist_fwp_parameters_as_string( $api_attr, $api_type );
 	
-	// get all api options
-	$attr_all = shortcode_atts( $localist_config['api_options']['all']['allowed'], $params, 'localist-calendar' );
+	// error processing
 
-	// store the api type as a variable
-	$api_type = $attr_all['get'];
-
-	// check that we have a valid 'get' type
-	if ( $api_type == '' || $api_type == null ) {
-
-		// let's default to events
-		$api_type = 'events';
-
-	}
-
-	// get the available api options (based on type) from the shortcode
-	$api_attr = shortcode_atts( $localist_config['api_options'][$api_type]['allowed'], $params, 'localist-calendar' );
-
-	// build the api string
-	$json_url = array(
-		'type' => $api_type
-		//'url' => plugins_url( '/sample/events.json', dirname(__FILE__) )
-	);
-
-	// get the matching api options by get type_url_form_file()
-	$parameters_string = usc_localist_fwp_parameters_as_string( $api_attr, $api_type );
-
-	if ( !empty( $parameters_string ) ) {
-		$json_url['options'] = $parameters_string;
-	}
-
-	// get the json data
-	$json_data = usc_localist_fwp_get_json( $json_url );
-
-	// check if we have json data
-	if ( $json_data ) {
-		
-		// check if we have an array
-		if ( is_array( $json_data ) ) {
-
-			// we have json array data
-
-			// TODO: function for looping through json data
-
-		} else if ( is_string( $json_data ) && '' != $json_data ) {
-
-			// output any error messages
-			return __( $json_data, 'usc-localist-for-wordpress');
+		// if we have any error messages
+		if ( !empty($parameters_string['message']) ) {
+			
+			$errors = true;
+			return __($parameters_string['message'], 'usc-localist-for-wordpress');
 
 		} else {
 
-			// output message that something needs to be addressed
-			return __( 'Ninja wildebeests have sprung into action. Please contact plugin development team.','textdomain' );
+			// no errors
+			$json_url['options'] = $parameters_string['parameters'];
+
 		}
-	}
+
+		// get the json data if no errors are present
+		if ( !$errors ) {
+
+			$json_data = usc_localist_fwp_get_json( $json_url );
+
+		}
+
+	// perform the api call
+
+		// check if we have json data
+		if ( !empty( $json_data ) ) {
+			
+			// check if we have an array
+			if ( is_array( $json_data ) ) {
+				
+				// we have json array data
+
+				// TODO: function for looping through json data
+
+			} else if ( is_string( $json_data ) && '' != $json_data ) {
+
+				// output any error messages
+				return __( $json_data, 'usc-localist-for-wordpress');
+
+			} else {
+
+				// output message that something needs to be addressed
+				return __( 'Ninja wildebeests have sprung into action. Please contact plugin development team.','textdomain' );
+			}
+		}
 }
 
 // add the shortcode function
