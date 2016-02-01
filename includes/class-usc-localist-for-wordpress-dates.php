274d43271fe5cec56df895da3d2afe8e0ad0d322
Jason Cauwels <troutacular@gmail.com>
2016-02-01 11:03:22 -0800

    dates as separate class; error fix for validation of integers

---
+++ includes/class-usc-localist-for-wordpress-dates.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Class: USC Localist for WordPress Dates
+ *
+ * Date functions and 
+ *
+ * @since      1.0.0
+ *
+ * @package    USC_Localist_for_WordPress
+ * @subpackage USC_Localist_for_WordPress/includes
+ */
+
+if ( ! class_exists( 'USC_Localist_For_Wordpress_Dates' ) ) {
+	
+	class USC_Localist_For_Wordpress_Dates {
+
+		/**
+		 * Error message array.
+		 * @var string
+		 */
+		private $config;
+
+		/**
+		 * Construct
+		 * =========
+		 *
+		 * @since 1.0.0
+		 * 
+		 * Constructor to run when the class is called.
+		 */
+		public function __construct() {
+
+			// require the config class for API variables
+			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-usc-localist-for-wordpress-config.php';
+
+			// retrun the API configurations
+			$this->config = USC_Localist_For_Wordpress_Config::$config;
+
+		}
+
+		/**
+		 * Validate Date
+		 * =============
+		 * 
+		 * Check if the date passed matches the intended format.
+		 * 
+		 * @param 	string 	$date 		date to pass for checking
+		 * @param 	string 	$format 	format to check against date
+		 * @return 	boolean	
+		 */
+		public static function validate_date( $date, $format = 'Y-m-d' ) {
+		    
+		    // returns new DateTime object formatted according to the specified format
+		    $d = DateTime::createFromFormat( $format, $date );
+
+		    // return boolean
+		    return $d && $d->format( $format ) == $date;
+		}
+
+		/**
+		 * Fix Date
+		 * ========
+		 * 
+		 * Change a valid date format to a specified format.
+		 * 
+		 * @param 	string 	$date 		valid date
+		 * @param 	string 	$format 	format which to change the date
+		 * @return 	string 				date in specified $format
+		 */
+		public static function fix_date( $date, $format = 'Y-m-d' ) {
+			
+			// change the $date to $format and return
+			$d = date( $format, strtotime( $date ) );
+			return $d;
+
+		}
+
+	}
+
+}
