beb3986cc5e3dc815a8abf653298ada82828a625
Jason Cauwels <troutacular@gmail.com>
2016-02-02 08:37:58 -0800

    activator/deactivator header comments

--- includes/class-usc-localist-for-wordpress-activator.php
+++ includes/class-usc-localist-for-wordpress-activator.php
@@ -3,11 +3,9 @@
 /**
  * Fired during plugin activation
  *
- * @link       http://example.com
- * @since      1.0.0
- *
- * @package    Plugin_Name
- * @subpackage Plugin_Name/includes
+ * @package    Usc_Localist_For_Wordpress
+ * @subpackage Usc_Localist_For_Wordpress/includes
+ * @author     USC Web Services <webhelp@usc.edu>
  */
 
 class USC_Localist_For_Wordpress_Activator {
