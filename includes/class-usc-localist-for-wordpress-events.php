735c0349ffdf30a308842a6f55b24a92944ca8cf
Jason Cauwels <troutacular@gmail.com>
2016-02-03 15:37:31 -0800

    events class init

---
+++ includes/class-usc-localist-for-wordpress-events.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Class: USC Localist for WordPress Events
+ * 
+ * A class to handle events displaying.  Accepts JSON array
+ * and checks template options for output format.
+ *
+ * @since 		1.0.0
+ * @package 	Usc_Localist_For_Wordpress
+ * @subpackage 	Usc_Localist_For_Wordpress/includes
+ * @author 		USC Web Services <webhelp@usc.edu>
+ */
+
+if ( ! class_exists( 'USC_Localist_For_Wordpress_Events' ) ) {
+
+	class USC_Localist_For_Wordpress_Events {
+
+		/**
+		 * Construct
+		 * =========
+		 *
+		 * @since 1.0.0
+		 * 
+		 * Constructor to run when the class is called.
+		 */
+		public function __construct() {
+
+
+		}
+
+	}
+
+}
