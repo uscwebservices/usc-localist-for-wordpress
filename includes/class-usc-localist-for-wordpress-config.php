b65d82263c8a111ec92b7a6dd1cea4e82d76aa9d
Jason Cauwels <troutacular@gmail.com>
2016-02-25 16:17:15 -0800

    config: add pagination parameter to allowed shortcodes

--- includes/class-usc-localist-for-wordpress-config.php
+++ includes/class-usc-localist-for-wordpress-config.php
@@ -71,6 +71,7 @@ if ( ! class_exists('USC_Localist_For_Wordpress_Config') ) {
 						'is_events_page' => '', // optional: indicate single events to display on the shortcode page
 						'template_multiple' => '', // opttional: slug name from templates custom post type - multiple events
 						'template_single' => '', // opttional: slug name from templates custom post type - single events
+						'paginate' => '', // optional: show pagination on multi-events
 
 						// localist values
 						'all_custom_fields' => '',
