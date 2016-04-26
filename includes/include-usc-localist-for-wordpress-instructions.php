c4bfda2959757f426efa795993a9788475aeea88
Jason Cauwels <troutacular@gmail.com>
2016-04-26 09:06:35 -0700

    add currently supported shortcode options to settings documentation

--- includes/include-usc-localist-for-wordpress-instructions.php
+++ includes/include-usc-localist-for-wordpress-instructions.php
@@ -34,6 +34,10 @@
 		padding: .25rem 0 0 1rem;
 	}
 
+	.usc_lfwp_wrap table ul {
+		padding: 0;
+	}
+
 </style>
 <div class="usc_lfwp_wrap">
 	
@@ -41,7 +45,11 @@
 
 	<ul>
 		<li><a href="#settings-bookmark-plugin-usage">Plugin Usage</a></li>
-		<li><a href="#settings-bookmark-api-options">API Options</a></li>
+		<li><a href="#settings-bookmark-shortcode-api-options">Shortcode API Options</a>
+			<ul>
+				<li><a href="#settings-bookmark-shortcode-api-options-locality">Locality</a></li>
+			</ul>
+		</li>
 		<li><a href="#settings-bookmark-custom-shortcode-api-options">Custom shortcode API options</a></li>
 		<li><a href="#settings-bookmark-customizer">Customizer</a>
 			<ul>
@@ -83,9 +91,68 @@
 	<p>This is a WordPress Plugin that uses the shortcode <code>[localist-calendar]</code> to get events from the <a href="https://calendar.usc.edu">USC Calendar</a>.</p>
 
 
-	<h3 id="settings-bookmark-api-options">API Options</h3>
+	<h3 id="settings-bookmark-shortcode-api-options">Shortcode API Options</h3>
+	
+	<p>Below are the attributes available from the Localist API. Please reference the <a href="http://www.localist.com/doc/api">Localist API</a> for full documentation of currently supported options.</p>
 
-	<p>Please reference the <a href="http://www.localist.com/doc/api">Localist API</a> for a full reference of current supported options.</p>
+	<table class="widefat">
+		<thead>
+			<td><strong>Location</strong></td>
+			<td><strong>Date</strong></td>
+			<td><strong>Page</strong></td>
+			<td><strong>Filter</strong></td>
+			<td><strong>Output</strong></td>
+		</thead>
+		<tr>
+			<td>
+				<ul>
+					<li><code>bounds</code></li>
+					<li><code>campus_id</code></li>
+					<li><code>group_id</code></li>
+					<li><code>near</code></li>
+					<li><code>units</code></li>
+					<li><code>venue_id</code></li>
+					<li><code>within</code></li>
+				</ul>
+			</td>
+			<td>
+				<ul>
+					<li><code>days</code></li>
+					<li><code>end</code></li>
+					<li><code>start</code></li>
+				</ul>
+			</td>
+			<td>
+				<ul>
+					<li><code>page</code></li>
+					<li><code>pp</code></li>
+				</ul>
+			</td>
+			<td>
+				<ul>
+					<li><code>created_after</code></li>
+					<li><code>created_before</code></li>
+					<li><code>exclude_type</code></li>
+					<li><code>featured</code></li>
+					<li><code>keyword</code></li>
+					<li><code>match</code></li>
+					<li><code>recurring</code></li>
+					<li><code>require_all</code></li>
+					<li><code>sponsored</code></li>
+					<li><code>type</code></li>
+				</ul>
+			</td>
+			<td>
+				<ul>
+					<li><code>all_custom_fields</code></li>
+					<li><code>direction</code></li>
+					<li><code>distinct</code></li>
+					<li><code>include_attendance</code></li>
+					<li><code>sort</code></li>
+				</ul>
+			</td>
+		</tr>
+	</table>
 
 	<h3 id="settings-bookmark-custom-shortcode-api-options">Custom Shortcode API options</h3>
 
