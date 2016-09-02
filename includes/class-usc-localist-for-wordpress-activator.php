414ce16db5677bb4ad96f309cc53e609b44e8eaf
Jason Cauwels <troutacular@gmail.com>
2016-09-02 11:53:26 -0700

    activator class doc sub package update

--- includes/class-usc-localist-for-wordpress-activator.php
+++ includes/class-usc-localist-for-wordpress-activator.php
@@ -3,7 +3,7 @@
  * USC Localist for WordPress Plugin Class.
  *
  * @package    Usc_Localist_For_Wordpress
- * @subpackage Usc_Localist_For_Wordpress/admin
+ * @subpackage Usc_Localist_For_Wordpress/includes
  * @author     USC Web Services <webhelp@usc.edu>
  */
 
