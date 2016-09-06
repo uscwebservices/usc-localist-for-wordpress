7fbfddcec802642abdc7d55298b6cdb70b81590e
Jason Cauwels <troutacular@gmail.com>
2016-09-06 08:19:11 -0700

    public class linted

--- public/class-usc-localist-for-wordpress-public.php
+++ public/class-usc-localist-for-wordpress-public.php
@@ -1,16 +1,21 @@
 <?php
 /**
- * The public-specific functionality of the plugin.
- *
- * Defines the plugin name, version, and initiates any public facing functions.
+ * USC Localist for WordPress Plugin Class.
  *
  * @package    Usc_Localist_For_Wordpress
- * @subpackage Usc_Localist_For_Wordpress/public
+ * @subpackage Usc_Localist_For_Wordpress/admin
  * @author     USC Web Services <webhelp@usc.edu>
  */
 
 if ( ! class_exists( 'USC_Localist_For_Wordpress_Public' ) ) {
-	
+
+	/**
+	 * The public-specific functionality of the plugin.
+	 *
+	 * Defines the plugin name, version, and initiates any public facing functions.
+	 *
+	 * @since 1.0.0
+	 */
 	class USC_Localist_For_Wordpress_Public {
 
 		/**
@@ -29,14 +34,14 @@ if ( ! class_exists( 'USC_Localist_For_Wordpress_Public' ) ) {
 		 * @access   private
 		 * @var      string    $version    The current version of this plugin.
 		 */
-		private $version;
+		public $version;
 
 		/**
 		 * Initialize the class and set its properties.
 		 *
 		 * @since    1.0.0
-		 * @param      string    $plugin_name       The name of this plugin.
-		 * @param      string    $version    The version of this plugin.
+		 * @param    string $plugin_name 		The name of this plugin.
+		 * @param    string $plugin_version		The version of this plugin.
 		 */
 		public function __construct( $plugin_name, $plugin_version ) {
 
@@ -49,8 +54,7 @@ if ( ! class_exists( 'USC_Localist_For_Wordpress_Public' ) ) {
 
 		/**
 		 * Load Dependencies
-		 * =================
-		 * 
+		 *
 		 * Load the required dependencies for this class.
 		 *
 		 * @since    1.0.0
@@ -58,14 +62,13 @@ if ( ! class_exists( 'USC_Localist_For_Wordpress_Public' ) ) {
 		 */
 		private function load_dependencies() {
 
-			// require the config class for API variables
+			// Require the config class for API variables.
 			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-usc-localist-for-wordpress-config.php';
 
-			// require the events shortcode Class
+			// Require the events shortcode Class.
 			require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-usc-localist-for-wordpress-shortcode.php';
 
-
-				// require the api class
+				// Require the api class.
 				require_once plugin_dir_path( dirname( __FILE__ ) ) . 'includes/class-usc-localist-for-wordpress-api.php';
 
 			$this->config = USC_Localist_For_Wordpress_Config::$config;
@@ -75,46 +78,42 @@ if ( ! class_exists( 'USC_Localist_For_Wordpress_Public' ) ) {
 
 		/**
 		 * Activate
-		 * ========
 		 *
 		 * Activate any functions that should run during the admin setup.
 		 *
 		 * @since 	1.0.0
 		 */
 		public function activate() {
-			
-			// register the custom post types
-			$this->custom_post_types();
-			
-		}
 
+			// Register the custom post types.
+			$this->custom_post_types();
+
+		}
 
 		/**
 		 * Add Query Variables Filter
-		 * ==========================
 		 *
-		 * Set 
+		 * Add the allowed URL query variables as an object from the allowed varibles stored in the config settings.
 		 *
 		 * @since 1.0.0
+		 * @param object $vars Existing URL variables object.
 		 */
-		public function add_query_variables_filter( $vars ){
-			
+		public function add_query_variables_filter( $vars ) {
+
 			$parameters = $this->config['url']['parameters'];
 
-			// loop through the available parameters from the config and add them
+			// Loop through the available parameters from the config and add them.
 			foreach ( $parameters as $value ) {
-				
-				// add the 'name' value to the allowed url parameter types
+
+				// Add the 'name' value to the allowed url parameter types.
 				$vars[] = $value['name'];
 
 			}
 
-			// return the $vars added
+			// Return the $vars added.
 			return $vars;
 
 		}
-
-		
 
 	}
 
