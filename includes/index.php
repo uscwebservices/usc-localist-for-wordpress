6ed064be004e31459d299817835d0407077d4fea
Jason Cauwels <troutacular@gmail.com>
2016-09-08 07:59:58 -0700

    rm sub package information on blank index

--- includes/index.php
+++ includes/index.php
@@ -3,8 +3,6 @@
  * USC Localist for WordPress Plugin.
  *
  * @package    Usc_Localist_For_Wordpress
- * @subpackage Usc_Localist_For_Wordpress/includes
- * @author     USC Web Services <webhelp@usc.edu>
  */
 
 /**
