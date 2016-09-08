e5ad87a9f0eda70f10de27e04e3aba5a9ae7f196
Jason Cauwels <troutacular@gmail.com>
2016-09-08 07:56:17 -0700

    blank index file updates for phpcs linting

--- includes/index.php
+++ includes/index.php
@@ -1,1 +1,12 @@
-<?php // Silence is golden
+<?php
+/**
+ * USC Localist for WordPress Plugin.
+ *
+ * @package    Usc_Localist_For_Wordpress
+ * @subpackage Usc_Localist_For_Wordpress/includes
+ * @author     USC Web Services <webhelp@usc.edu>
+ */
+
+/**
+ * Silence is golden.
+ */
